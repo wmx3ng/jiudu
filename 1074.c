eb984f45bf0b7027ed178c63a37dc3bdb2e7c53f
Wang Mingxing <wmx3ng@gmail.com>
2014-05-11 18:14:02 +0800

    commit problem 1074 and it was received.

--- 1074.c
+++ 1074.c
@@ -2,8 +2,30 @@
 #include<stdio.h>
 #include<string.h>
 
+int judgeBal(char *ch)
+{
+	char *start=ch;
+	char *end=start+(strlen(ch)-1);
+	while(start<end)
+	{
+		if(*start!=*end)
+			return 0;
+		start++;
+		end--;
+	}
+	return 1;		
+}
+
 int main()
 {
-
-  return 0;
+	int n=256;
+	int i;
+	char ch[256];
+	for (i=0;i<256;i++)
+	{
+		sprintf(ch,"%d",i*i);
+	        if(judgeBal(ch))
+			printf("%d\n",i);
+	}
+	return 0;
 }
